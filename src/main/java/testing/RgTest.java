a1d73bf9b13f1d69fccf6cb19b3daafba68231d3
Diego Didona <didona@gsd.inesc-id.pt>
2013-10-13 01:14:34 +0100

    Rg test completed

--- src/main/java/testing/RgTest.java
+++ src/main/java/testing/RgTest.java
@@ -2,6 +2,10 @@ package testing;
 
 import CsvOracles.RadargunCsvInputOracle;
 import CsvOracles.params.CsvRgParams;
+import eu.cloudtm.autonomicManager.commons.EvaluatedParam;
+import eu.cloudtm.autonomicManager.commons.ForecastParam;
+import eu.cloudtm.autonomicManager.commons.IsolationLevel;
+import eu.cloudtm.autonomicManager.commons.ReplicationProtocol;
 import org.apache.log4j.PropertyConfigurator;
 
 
@@ -17,5 +21,16 @@ public class RgTest {
    public static void main(String[] args) throws Exception {
       PropertyConfigurator.configure("conf/log4j.properties");
       RadargunCsvInputOracle rg = new RadargunCsvInputOracle(new CsvRgParams(test));
+      rg.setForecastParam(ForecastParam.NumNodes, 5);
+      rg.setForecastParam(ForecastParam.ReplicationDegree, 5);
+      rg.setForecastParam(ForecastParam.ReplicationProtocol, ReplicationProtocol.TWOPC);
+      rg.setEvaluatedParam(EvaluatedParam.ISOLATION_LEVEL, IsolationLevel.RR);
+      //Here you create your oracle
+      //Oracle o = new Oracle_IMPL();
+      //Invoke
+      //OutputOracle_IMPl = o.forecast(rg)
+      //Print
+      //System.out.println(rg.throughput(0)+ " "+rg.responseTime(1)+ " "+rg.abortRate(1));
+
    }
 }
